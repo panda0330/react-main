a02dc20f07bd428749028eefb1d81df83b0656b2
panda0330 <superdev330@gmail.com>
2023-11-07 14:28:17 +0900

    server.js -2

--- fixtures/ssr2/server/server.js
+++ fixtures/ssr2/server/server.js
@@ -25,3 +25,48 @@ const {JS_BUNDLE_DELAY} = require('./delays');
 const PORT = process.env.PORT || 4000;
 const app = express();
 
+app.use((req, res, next) => {
+  if (req.url.endsWith('.js')) {
+    // Artificially delay serving JS
+    // to demonstrate streaming HTML.
+    setTimeout(next, JS_BUNDLE_DELAY);
+  } else {
+    next();
+  }
+});
+
+app.use(compress());
+app.get(
+  '/',
+  handleErrors(async function (req, res) {
+    await waitForWebpack();
+    render(req.url, res);
+  })
+);
+app.use(express.static('build'));
+app.use(express.static('public'));
+
+app
+  .listen(PORT, () => {
+    console.log(`Listening at ${PORT}...`);
+  })
+  .on('error', function (error) {
+    if (error.syscall !== 'listen') {
+      throw error;
+    }
+    const isPipe = portOrPipe => Number.isNaN(portOrPipe);
+    const bind = isPipe(PORT) ? 'Pipe ' + PORT : 'Port ' + PORT;
+    switch (error.code) {
+      case 'EACCES':
+        console.error(bind + ' requires elevated privileges');
+        process.exit(1);
+        break;
+      case 'EADDRINUSE':
+        console.error(bind + ' is already in use');
+        process.exit(1);
+        break;
+      default:
+        throw error;
+    }
+  });
+
