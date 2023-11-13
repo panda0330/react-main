5282e12ebb5f9806e0e50dc10463d08a221b46f1
panda0330 <superdev330@gmail.com>
2023-11-13 20:36:55 +0900

    server.js change

--- fixtures/ssr2/server/server.js
+++ fixtures/ssr2/server/server.js
@@ -70,3 +70,15 @@ app
     }
   });
 
+function handleErrors(fn) {
+  return async function (req, res, next) {
+    try {
+      return await fn(req, res);
+    } catch (x) {
+      next(x);
+    }
+  };
+}
+
+
+}
