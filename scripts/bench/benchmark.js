bbaa7223c0ae2b094319b1f61dfa82b142365980
panda0330 <superdev330@gmail.com>
2023-12-18 16:30:33 +0900

    change benchmark

--- scripts/bench/benchmark.js
+++ scripts/bench/benchmark.js
@@ -61,6 +61,21 @@ function calculateAverages(runs) {
   const data = [];
   const averages = [];
 
+  runs.forEach((entries, x) => {
+    entries.forEach(({entry, time}, i) => {
+      if (i >= averages.length) {
+        data.push([time]);
+        averages.push({
+          entry,
+          mean: 0,
+          sem: 0,
+        });
+      } else {
+        data[i].push(time);
+        if (x === runs.length - 1) {
+          const dataWithoutOutliers = stats.filterMADoutliers(data[i]);
+          averages[i].mean = stats.mean(dataWithoutOutliers);
+          averages[i].sem = calculateStandardErrorOfMean(data[i]);
 
 }
 
