13d675eaeb218d6b92ef30f59ba522eb3dc0f282
panda0330 <superdev330@gmail.com>
2023-12-22 10:01:36 +0900

    benchmark.js change

--- scripts/bench/benchmarks/hacker-news/benchmark.js
+++ scripts/bench/benchmarks/hacker-news/benchmark.js
@@ -273,3 +273,32 @@
     );
   }
 
+  function App({stories}) {
+    return e(
+      'center',
+      null,
+      e(
+        'table',
+        {
+          id: 'hnmain',
+          border: 0,
+          cellPadding: 0,
+          cellSpacing: 0,
+          width: '85%',
+          style: {
+            'background-color': '#f6f6ef',
+          },
+        },
+        e(
+          'tbody',
+          null,
+          e(HeaderBar, null),
+          e('tr', {height: 10}),
+          e(StoryList, {
+            stories,
+          })
+        )
+      )
+    );
+  }
+
